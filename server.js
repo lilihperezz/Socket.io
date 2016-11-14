ae9d197d466883d777ccf0db6eed9453b9f2c4a0
lili hualtibamba <lilihperezz@gmail.com>
2016-11-14 11:47:24 -0500

    agregando archivos

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ var app = express();
 var server = require('http').createServer(app);
 //creamos var io tendrá todas las funcionalidades de los sockets
 var io = require('socket.io')(server);
+//Para usar la parte publica de ficheros estáticos
+ app.use(express.static('public'));
 //app donde está express cuando reciba un get en la ruta raiz active la sgt acción
  //mande un estatus ok y que envie msj.
 app.get('/',function(req, res){
 	res.status(200).send("Hello World :)");
 });
+// escuchar mensaje del navegador o servidor ,msj viene del navegador(html)
+io.on('connection',function(socket){
+ console.log("Alguien se ha conectado con sockets");
+});
 
-//probando
+//probando 
 server.listen(8080,function(){
 	console.log("servidor encendido en http://localhost:8080");
 })
